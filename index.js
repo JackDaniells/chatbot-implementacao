3b2afde5481e72447bd71b38b06987e72ecf889e
jackdaniells <danielk.kock@live.com>
2017-02-08 18:28:08 -0200

    change app id and password

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 var connector = new builder.ChatConnector({
     /*appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD*/
-    appId: '',
-    appPassword: ''
+    appId: '4bacbab9-c056-4979-b5e9-264423dbc522',
+    appPassword: '7kLJwMTJ4VizNSTtPCe9C0o'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
