cc8f2b5ab4ce67003b05f51077792053f33e39e5
jackdaniells <danielk.kock@live.com>
2017-02-08 20:20:15 -0200

    debug about session

--- app.js
+++ app.js
@@ -74,7 +74,6 @@ bot.dialog('rootMenu', [
             case 1:
             	session.beginDialog('about');
             default:
-            	session.send('Até mais :)');
             	clearSession(session);
                 session.endDialog();
                 break;
@@ -86,44 +85,44 @@ bot.dialog('rootMenu', [
     }
 ]).reloadAction('showMenu', null, { matches: /^(menu|back)/i });
 
-bot.dialog('about'), function(session){
-	session.send('Uros é um sistema inteligente de controle de acesso! Além de contar com wifi e protocolo Uros de comunicação sem fio entre dispositivos, ele aprende com o uso e pode indicar ações de melhorias e segurança.');
-	session.send('A solução oferecida é composta por:');
-	        var msg = new builder.Message(session)
-            .attachmentLayout(builder.AttachmentLayout.carousel)
-            .attachments([
-                new builder.HeroCard(session)
-                    .title("Central")
-                    .subtitle("A central é o cérebro do sistema Uros, fazendo a integração entre os dispositivos, como sensores, fechaduras e leitoras de cartão, ela também se conecta a internet, permitindo controlar tudo isso online.")
-                    .images([
-                        builder.CardImage.create(session, "http://uros.cloud/images/blog/1.jpg")
-                            .tap(builder.CardAction.showImage(session, "http://uros.cloud/images/blog/1.jpg")),
-                    ]),
-                new builder.HeroCard(session)
-                    .title("Sensor de Presença")
-                    .subtitle("Sensor de presença sem fio do tipo infravermelho. Pode trabalhar em conjunto com outros sensores, para criar redundâncias ou perfil de eventos.")
-                    .images([
-                        builder.CardImage.create(session, "http://uros.cloud/images/blog/3.jpg")
-                            .tap(builder.CardAction.showImage(session, "http://uros.cloud/images/blog/3.jpg")),
-                    ]),
-                new builder.HeroCard(session)
-                    .title("Leitor de Cartão")
-                    .subtitle("Leitor de identificação de cartão ou Tags RFID sem fio, com instalação descomplicada, permitindo que o leitor seja posicionado em portas de vidro, colunas, sem uso de fios.")
-                    .images([
-                        builder.CardImage.create(session, "http://uros.cloud/images/blog/2.jpg")
-                            .tap(builder.CardAction.showImage(session, "http://uros.cloud/images/blog/2.jpg"))
-                    ]),
-                    new builder.HeroCard(session)
-                    .title("Fechadura Eletrônica")
-                    .subtitle("Atuador da fechadura. Trata-se de um relé sem fio, que aciona a fechadura da porta. Pode ser utilizada com uma vasta variedade de fechaduras encontradas no mercado.")
-                    .images([
-                        builder.CardImage.create(session, "http://uros.cloud/images/blog/4.jpg")
-                            .tap(builder.CardAction.showImage(session, "http://uros.cloud/images/blog/4.jpg"))
-                    ])
-            ]);
+bot.dialog('about', function(session){
+		session.send('Uros é um sistema inteligente de controle de acesso! Além de contar com wifi e protocolo Uros de comunicação sem fio entre dispositivos, ele aprende com o uso e pode indicar ações de melhorias e segurança.');
+		session.send('A solução oferecida é composta por:');
+		        var msg = new builder.Message(session)
+	            .attachmentLayout(builder.AttachmentLayout.carousel)
+	            .attachments([
+	                new builder.HeroCard(session)
+	                    .title("Central")
+	                    .subtitle("A central é o cérebro do sistema Uros, fazendo a integração entre os dispositivos, como sensores, fechaduras e leitoras de cartão, ela também se conecta a internet, permitindo controlar tudo isso online.")
+	                    .images([
+	                        builder.CardImage.create(session, "http://uros.cloud/images/blog/1.jpg")
+	                            .tap(builder.CardAction.showImage(session, "http://uros.cloud/images/blog/1.jpg")),
+	                    ]),
+	                new builder.HeroCard(session)
+	                    .title("Sensor de Presença")
+	                    .subtitle("Sensor de presença sem fio do tipo infravermelho. Pode trabalhar em conjunto com outros sensores, para criar redundâncias ou perfil de eventos.")
+	                    .images([
+	                        builder.CardImage.create(session, "http://uros.cloud/images/blog/3.jpg")
+	                            .tap(builder.CardAction.showImage(session, "http://uros.cloud/images/blog/3.jpg")),
+	                    ]),
+	                new builder.HeroCard(session)
+	                    .title("Leitor de Cartão")
+	                    .subtitle("Leitor de identificação de cartão ou Tags RFID sem fio, com instalação descomplicada, permitindo que o leitor seja posicionado em portas de vidro, colunas, sem uso de fios.")
+	                    .images([
+	                        builder.CardImage.create(session, "http://uros.cloud/images/blog/2.jpg")
+	                            .tap(builder.CardAction.showImage(session, "http://uros.cloud/images/blog/2.jpg"))
+	                    ]),
+	                    new builder.HeroCard(session)
+	                    .title("Fechadura Eletrônica")
+	                    .subtitle("Atuador da fechadura. Trata-se de um relé sem fio, que aciona a fechadura da porta. Pode ser utilizada com uma vasta variedade de fechaduras encontradas no mercado.")
+	                    .images([
+	                        builder.CardImage.create(session, "http://uros.cloud/images/blog/4.jpg")
+	                            .tap(builder.CardAction.showImage(session, "http://uros.cloud/images/blog/4.jpg"))
+	                    ])
+	    ]);
         session.send(msg);
-        session.beginDialog('rootMenu');
-};
+       
+});
 
 
 bot.dialog('login',[
